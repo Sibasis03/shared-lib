93114cd5cfea26b4aa801d5b58d61f1babee7164
Sibasis Gouda <sibasisg@webelight.co.in>
2025-02-11 11:43:55 +0530

    added

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,4 +1,13 @@
 def call(String DOCKER_IMAGE) {
-    sh "docker build -t ${DOCKER_IMAGE} ."
-
+    try {
+        sh "docker build -t ${DOCKER_IMAGE} ."
+    } catch (Exception e) {
+        echo "Error building Docker image: ${DOCKER_IMAGE}. Details: ${e.getMessage()}"
+        
+        def imageExists = sh(script: "docker images -q ${DOCKER_IMAGE}", returnStatus: true) == 0
+        if (!imageExists) {
+            echo "Docker image ${DOCKER_IMAGE} not found."
+            currentBuild.result = 'FAILURE'
+        }
+    }
 }
--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -2,3 +2,16 @@ def call(String branch, String credentialsId = 'gitlab-https-credentials') {
     def config = repoUrl()
     git branch: "${branch}", url: "${config.repoUrl}", credentialsId: credentialsId
 }
+
+// Method to clone the repository with error handling
+def call(String branch, String credentialsId = 'gitlab-https-credentials') {
+    def config = repoUrl()
+
+    try {
+        echo "Cloning repository from ${config.repoUrl} on branch ${branch}"
+        git branch: "${branch}", url: "${config.repoUrl}", credentialsId: credentialsId
+    } catch (Exception e) {
+        echo "Error cloning repository from ${config.repoUrl}. Details: ${e.getMessage()}"
+        currentBuild.result = 'FAILURE'
+    }
+}
