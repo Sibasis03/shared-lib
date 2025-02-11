7ba333d3490da2fe97a5ec1475ccf084645680d1
Sibasis Gouda <sibasisg@webelight.co.in>
2025-02-11 12:49:24 +0530

    commit

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,5 +1,13 @@
 def call(String DOCKER_REGISTRY_CREDENTIALS_ID, String DOCKER_IMAGE) {
-    docker.withRegistry('https://index.docker.io/v1/', DOCKER_REGISTRY_CREDENTIALS_ID) {
-        docker.image(DOCKER_IMAGE).push()
+    try {
+        docker.withRegistry('https://index.docker.io/v1/', DOCKER_REGISTRY_CREDENTIALS_ID) {
+            echo "Pushing Docker image ${DOCKER_IMAGE} to Docker registry..."
+            docker.image(DOCKER_IMAGE).push()
+        }
+        echo "Docker image ${DOCKER_IMAGE} pushed successfully."
+    } catch (Exception e) {
+        echo "Error occurred while pushing Docker image ${DOCKER_IMAGE} to Docker registry: ${e.getMessage()}"
+        currentBuild.result = 'FAILURE'  
+        throw e
     }
 }
