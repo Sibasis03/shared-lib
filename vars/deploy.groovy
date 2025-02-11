7a0d71083f3ca057242c41cbcdd6f06fdacd80c1
Sibasis Gouda <sibasisg@webelight.co.in>
2025-02-11 12:35:20 +0530

    deploy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -7,7 +7,7 @@ def call(String git_hash) {
             docker-compose -f docker-compose.yml down  # Stop and remove existing containers
             docker-compose -f docker-compose.yml up -d  # Deploy using the Git hash directly as the image tag
         """
-        echo "Deployment successful with Git hash: ${githash}"
+        echo "Deployment successful with Git hash: ${git_hash}"
     } catch (Exception e) {
         echo "Deployment failed: ${e.getMessage()}"
         currentBuild.result = 'FAILURE'
