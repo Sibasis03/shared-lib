fca2a81e07576c379caf29343f15002996104c1e
Sibasis Gouda <sibasisg@webelight.co.in>
2025-02-12 17:10:15 +0530

    added

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -2,16 +2,18 @@ def call() {
     try {
         withCredentials([string(credentialsId: 'sonar_url', variable: 'SONAR_URL')]) {
             withCredentials([string(credentialsId: 'sonarqube', variable: 'SONAR_AUTH_TOKEN')]) {
-                sh 'mvn sonar:sonar -Dsonar.login=$SONAR_AUTH_TOKEN -Dsonar.host.url=${SONAR_URL}'
+                withSonarQubeEnv('SonarQube') { 
+                    sh 'mvn sonar:sonar -Dsonar.login=$SONAR_AUTH_TOKEN -Dsonar.host.url=${SONAR_URL}'
+                }
             }
         }
 
-        // script {
-            def qgStatus = waitForQualityGate() // This needs to be in a script block
+        script {
+            def qgStatus = waitForQualityGate() 
             if (qgStatus.status != 'OK') {
                 error "Quality Gate failed: ${qgStatus.status}"
             }
-        // }
+        }
 
     } catch (Exception e) {
         echo "Error occurred during static code analysis: ${e.getMessage()}"
