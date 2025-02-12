30cc08c03a829dedcd8f716fcfd41f1db46974e1
Sibasis Gouda <sibasisg@webelight.co.in>
2025-02-12 16:55:19 +0530

    added

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -6,12 +6,12 @@ def call() {
             }
         }
 
-        script {
-            def qgStatus = waitForQualityGate() // This needs to be in a script block
-            if (qgStatus.status != 'OK') {
-                error "Quality Gate failed: ${qgStatus.status}"
-            }
-        }
+        // script {
+        //     def qgStatus = waitForQualityGate() // This needs to be in a script block
+        //     if (qgStatus.status != 'OK') {
+        //         error "Quality Gate failed: ${qgStatus.status}"
+        //     }
+        // }
 
     } catch (Exception e) {
         echo "Error occurred during static code analysis: ${e.getMessage()}"
