c8ff02cb76d1ecd3957ea2d7b2031bba6042a384
Sibasis Gouda <sibasisg@webelight.co.in>
2025-02-12 16:50:18 +0530

    added

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -5,6 +5,14 @@ def call() {
                 sh 'mvn sonar:sonar -Dsonar.login=$SONAR_AUTH_TOKEN -Dsonar.host.url=${SONAR_URL}'
             }
         }
+
+        script {
+            def qgStatus = waitForQualityGate() // This needs to be in a script block
+            if (qgStatus.status != 'OK') {
+                error "Quality Gate failed: ${qgStatus.status}"
+            }
+        }
+
     } catch (Exception e) {
         echo "Error occurred during static code analysis: ${e.getMessage()}"
         currentBuild.result = 'FAILURE' 
