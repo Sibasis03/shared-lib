4b35a35b5f7c3b22e7e2241fe96a4324345d8ab8
Sibasis Gouda <sibasisg@webelight.co.in>
2025-02-13 17:03:42 +0530

    added

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,25 +1,45 @@
+// def call() {
+//     try {
+//         withCredentials([string(credentialsId: 'sonar_url', variable: 'SONAR_URL')]) {
+//             withCredentials([string(credentialsId: 'sonarqube', variable: 'SONAR_AUTH_TOKEN')]) {
+//                 withSonarQubeEnv('sonarqube') { 
+//                     sh 'mvn sonar:sonar -Dsonar.login=$SONAR_AUTH_TOKEN -Dsonar.host.url=${SONAR_URL}'
+//                 }
+//             }
+//         }
+
+//         // timeout(time: 3, unit: 'MINUTES') {
+//         script {
+//             def qgStatus = waitForQualityGate()
+//             if (qgStatus.status != 'OK') {
+//                 error "Quality Gate failed: ${qgStatus.status}"
+//             }
+//         // }
+//     }
+
+//     } catch (Exception e) {
+//         echo "Error occurred during static code analysis: ${e.getMessage()}"
+//         currentBuild.result = 'FAILURE' 
+//         throw e 
+//     }
+// }
+
+
 def call() {
-    try {
-        withCredentials([string(credentialsId: 'sonar_url', variable: 'SONAR_URL')]) {
-            withCredentials([string(credentialsId: 'sonarqube', variable: 'SONAR_AUTH_TOKEN')]) {
-                withSonarQubeEnv('sonarqube') { 
-                    sh 'mvn sonar:sonar -Dsonar.login=$SONAR_AUTH_TOKEN -Dsonar.host.url=${SONAR_URL}'
-                }
+    withCredentials([string(credentialsId: 'sonar_url', variable: 'SONAR_URL')]) {
+        withCredentials([string(credentialsId: 'sonarqube', variable: 'SONAR_AUTH_TOKEN')]) {
+            
+            withSonarQubeEnv('sonarqube') { 
+                sh """
+                    ${tool 'sonarqube'}/bin/sonar-scanner \
+                    -Dsonar.projectKey=${JOB_BASE_NAME} \
+                    -Dsonar.sources=${env.WORKSPACE}/src/main/java \
+                    -Dsonar.java.binaries=${env.WORKSPACE}/target/classes \
+                    -Dsonar.host.url=${SONAR_URL} \
+                    -Dsonar.token=${SONAR_AUTH_TOKEN} \
+                    -Dsonar.qualitygate.wait=true
+                """
             }
         }
-
-        // timeout(time: 3, unit: 'MINUTES') {
-        script {
-            def qgStatus = waitForQualityGate()
-            if (qgStatus.status != 'OK') {
-                error "Quality Gate failed: ${qgStatus.status}"
-            }
-        // }
-    }
-
-    } catch (Exception e) {
-        echo "Error occurred during static code analysis: ${e.getMessage()}"
-        currentBuild.result = 'FAILURE' 
-        throw e 
     }
 }
