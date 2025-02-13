3713b71ca6861849f4c217bfc12ec26f3eae2c5b
Sibasis Gouda <sibasisg@webelight.co.in>
2025-02-13 17:39:45 +0530

    added

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -30,16 +30,18 @@ def call() {
         withCredentials([string(credentialsId: 'sonarqube', variable: 'SONAR_AUTH_TOKEN')]) {
             
             withSonarQubeEnv('sonarqube') { 
+                // Use environment variables securely without direct Groovy interpolation
                 sh """
                     ${tool 'sonarqube-scanner'}/bin/sonar-scanner \
                     -Dsonar.projectKey=${JOB_BASE_NAME} \
                     -Dsonar.sources=${env.WORKSPACE_PATH_JENKINS_SERVER}/src \
                     -Dsonar.java.binaries=${env.WORKSPACE_PATH_JENKINS_SERVER}/target/classes \
-                    -Dsonar.host.url=${SONAR_URL} \
-                    -Dsonar.token=${SONAR_AUTH_TOKEN} \
+                    -Dsonar.host.url=$SONAR_URL \
+                    -Dsonar.token=$SONAR_AUTH_TOKEN \
                     -Dsonar.qualitygate.wait=true
                 """
             }
         }
     }
 }
+
