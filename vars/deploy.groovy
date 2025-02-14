2ec0ff083f878b3724cdc465a59ca3239ebde57f
Sibasis Gouda <sibasisg@webelight.co.in>
2025-02-14 18:15:49 +0530

    added

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,16 +1,31 @@
 def call(String git_hash) {
     echo "Deploying with Git hash: ${git_hash}"
 
-    try {
-        sh """
-            export githash=${git_hash}
-            docker-compose -f docker-compose.yml down  # Stop and remove existing containers
-            docker-compose -f docker-compose.yml up -d  # Deploy using the Git hash directly as the image tag
-        """
-        echo "Deployment successful with Git hash: ${git_hash}"
-    } catch (Exception e) {
-        echo "Deployment failed: ${e.getMessage()}"
-        currentBuild.result = 'FAILURE'
-        throw e
+    sshagent(['34.220.195.192']) { 
+
+        environment {
+            REMOTE_USER = 'ubuntu'
+            REMOTE_SERVER = '34.220.195.192'
+        }
+
+        script {
+            sh """
+                ssh -o StrictHostKeyChecking=no \${REMOTE_USER}@\${REMOTE_SERVER} \
+                'cd /home/ubuntu/app && echo "Current Directory: \$(pwd)" && docker-compose down && rm -rf *.yml'
+            """
+
+            sh """
+                if [ -f \${WORKSPACE}/docker-compose.yml ]; then
+                    scp -o StrictHostKeyChecking=no \${WORKSPACE}/docker-compose.yml \${REMOTE_USER}@\${REMOTE_SERVER}:/home/ubuntu/app/
+                else
+                    echo "docker-compose.yml not found!"
+                fi
+                """
+            sh """
+                ssh -o StrictHostKeyChecking=no \${REMOTE_USER}@\${REMOTE_SERVER} \
+                'cd /home/ubuntu/app && export githash="${githash}" && echo "Git Hash is \$githash" && docker-compose up -d'
+            """
+        }
     }
 }
+
