6198debbf4acec753b17eb7382473e10bb6756f4
Sibasis Gouda <sibasisg@webelight.co.in>
2025-02-18 17:01:26 +0530

    added

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,6 @@
 def call(String git_hash) {
     sh """
-        ssh -o StrictHostKeyChecking=no \${env.REMOTE_USER}@\${env.REMOTE_SERVER} \
+        ssh -o StrictHostKeyChecking=no \${REMOTE_USER}@\${REMOTE_SERVER} \
         'if [ ! -d /home/ubuntu/app ]; then \
         mkdir -p /home/ubuntu/app; \
         fi && \
@@ -9,13 +9,13 @@ def call(String git_hash) {
 
     sh """
         if [ -f \${WORKSPACE}/docker-compose.yml ]; then
-            scp -o StrictHostKeyChecking=no \${WORKSPACE}/docker-compose.yml \${env.REMOTE_USER}@\${env.REMOTE_SERVER}:/home/ubuntu/app/
+            scp -o StrictHostKeyChecking=no \${WORKSPACE}/docker-compose.yml \${REMOTE_USER}@\${REMOTE_SERVER}:/home/ubuntu/app/
         else
             echo "docker-compose.yml not found!"
         fi
     """
     sh """
-        ssh -o StrictHostKeyChecking=no \${env.REMOTE_USER}@\${env.REMOTE_SERVER} \
+        ssh -o StrictHostKeyChecking=no \${REMOTE_USER}@\${REMOTE_SERVER} \
         'cd /home/ubuntu/app && export githash="${githash}" && echo "Git Hash is \$githash" && docker-compose up -d'
     """
     echo "Deploying with Git hash: ${git_hash}"
