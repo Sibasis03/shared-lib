5893b50f87140eb984d7b0ef7060e8a6d9cb032a
Sibasis Gouda <sibasisg@webelight.co.in>
2025-02-18 16:42:09 +0530

    added

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,31 +1,23 @@
 def call(String git_hash) {
+    sh """
+        ssh -o StrictHostKeyChecking=no \${env.REMOTE_USER}@\${env.REMOTE_SERVER} \
+        'if [ ! -d /home/ubuntu/app ]; then \
+        mkdir -p /home/ubuntu/app; \
+        fi && \
+        cd /home/ubuntu/app && echo "Current Directory: \$(pwd)" && rm -rf *.yml'
+    """
+
+    sh """
+        if [ -f \${WORKSPACE}/docker-compose.yml ]; then
+            scp -o StrictHostKeyChecking=no \${WORKSPACE}/docker-compose.yml \${env.REMOTE_USER}@\${env.REMOTE_SERVER}:/home/ubuntu/app/
+        else
+            echo "docker-compose.yml not found!"
+        fi
+    """
+    sh """
+        ssh -o StrictHostKeyChecking=no \${env.REMOTE_USER}@\${env.REMOTE_SERVER} \
+        'cd /home/ubuntu/app && export githash="${githash}" && echo "Git Hash is \$githash" && docker-compose up -d'
+    """
     echo "Deploying with Git hash: ${git_hash}"
-
-    sshagent(['34.220.195.192']) { 
-
-        environment {
-            REMOTE_USER = 'ubuntu'
-            REMOTE_SERVER = '34.220.195.192'
-        }
-
-        script {
-            sh """
-                ssh -o StrictHostKeyChecking=no \${REMOTE_USER}@\${REMOTE_SERVER} \
-                'cd /home/ubuntu/app && echo "Current Directory: \$(pwd)" && docker-compose down && rm -rf *.yml'
-            """
-
-            sh """
-                if [ -f \${WORKSPACE}/docker-compose.yml ]; then
-                    scp -o StrictHostKeyChecking=no \${WORKSPACE}/docker-compose.yml \${REMOTE_USER}@\${REMOTE_SERVER}:/home/ubuntu/app/
-                else
-                    echo "docker-compose.yml not found!"
-                fi
-                """
-            sh """
-                ssh -o StrictHostKeyChecking=no \${REMOTE_USER}@\${REMOTE_SERVER} \
-                'cd /home/ubuntu/app && export githash="${githash}" && echo "Git Hash is \$githash" && docker-compose up -d'
-            """
-        }
-    }
 }
 
--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,30 +1,3 @@
-// def call() {
-//     try {
-//         withCredentials([string(credentialsId: 'sonar_url', variable: 'SONAR_URL')]) {
-//             withCredentials([string(credentialsId: 'sonarqube', variable: 'SONAR_AUTH_TOKEN')]) {
-//                 withSonarQubeEnv('sonarqube') { 
-//                     sh 'mvn sonar:sonar -Dsonar.login=$SONAR_AUTH_TOKEN -Dsonar.host.url=${SONAR_URL}'
-//                 }
-//             }
-//         }
-
-//         // timeout(time: 3, unit: 'MINUTES') {
-//         script {
-//             def qgStatus = waitForQualityGate()
-//             if (qgStatus.status != 'OK') {
-//                 error "Quality Gate failed: ${qgStatus.status}"
-//             }
-//         // }
-//     }
-
-//     } catch (Exception e) {
-//         echo "Error occurred during static code analysis: ${e.getMessage()}"
-//         currentBuild.result = 'FAILURE' 
-//         throw e 
-//     }
-// }
-
-
 def call() {
     withCredentials([string(credentialsId: 'sonar_url', variable: 'SONAR_URL')]) {
         withCredentials([string(credentialsId: 'sonarqube', variable: 'SONAR_AUTH_TOKEN')]) {
